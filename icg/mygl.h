7f747da0057fd73a68554085ae96638eb85677d5
Lucas Henrique <32209223+LukasHenrique@users.noreply.github.com>
2020-06-24 14:44:11 -0300

    Add files via upload

---
+++ icg/mygl.h
@@ -0,0 +1,18 @@
+#ifndef MYGL_H
+#define MYGL_H
+
+#include "core.h"
+#include "frame_buffer.h"
+
+// Declaração da função que chamará as funções implementadas pelo aluno
+void MyGlDraw(void);
+
+int posicaoPixel();
+
+void putPixel();
+
+void drawLine();
+
+void drawTriangle();
+
+#endif  // MYGL_H
